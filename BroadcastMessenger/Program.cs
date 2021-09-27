9861cd234f5eff975bfc46776249371aaa81876c
Никита Антоненко <nikita.dmitrovich@gmail.com>
2021-09-27 22:49:36 +0300

    Add project files.

---
+++ BroadcastMessenger/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using ClientServerLib;
+
+
+namespace BroadcastMessenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server("127.0.0.1",8000);
+            Task NewClientsChecker = new Task(() => { while (true) { server.AddClient("127.0.0.1", 8000); } });
+            server.Start();
+            NewClientsChecker.Start();
+            try
+            {
+
+                while (true) {
+
+                    if (server.handler.Count != 0) {
+                        Console.Write("Send Message(Enter path to txt file to send it):");
+                        var message = Console.ReadLine();
+                        if (File.Exists(message)) {
+                            if (new FileInfo(message).Extension == ".txt")
+                            {
+                                message = "-text-" + new FileInfo(message).Name + "-" + File.ReadAllText(message);
+                            }
+                        }
+                        for (int i = 0; i < server.handler.Count-1; i++) {
+                            server.Send(Server.FromStringToBytes(message), i);
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+            server.Close();
+
+
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using ClientServerLib;
+
+namespace ClientConsole
+{
+    class Program
+    {
+        static void FileCreateor(string content) {
+            
+                File.Create(content.Split('-')[2]).Close();
+                File.WriteAllText(content.Split('-')[2], content.Remove(0, content.Split('-')[1].Length + content.Split('-')[2].Length+3));
+            
+        }
+        static void Main(string[] args)
+        {
+            Client client = new Client("127.0.0.1",8000);
+            client.Connect();
+
+            try
+            {
+                while (true) {
+                    var data = client.Get();
+
+                    string checker = Client.FromBytesToString(data);
+                    if (checker.Split('-').Length >= 2)
+                    {
+                        if (checker.Split('-')[1] == "text")
+                        {
+                            FileCreateor(checker);
+                            Console.WriteLine("File loaded!");
+                            continue;
+                        }
+                    }
+
+                    Console.WriteLine(DateTime.Now.ToShortTimeString()+":"+Client.FromBytesToString(data));
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            client.Close();
+        }
+    }
+}
